16d6afa00cb6680cbacb95b51d5ae0a9714d44d6
Leonid <leonid_95@yahoo.de>
2017-12-09 17:05:15 +0100

    IT'S ALIVE!

--- Assets/Papparazzi.cs
+++ Assets/Papparazzi.cs
@@ -7,7 +7,9 @@ public class Papparazzi : MonoBehaviour {
     Rigidbody body;
     Quaternion originalRotation;
     Vector3 originalPosition;
-    private bool canMove;
+    private int shots;
+    public int maxshots = 3;
+    private bool canMove = false;
     public float sensitivityX = 15F;
     public float sensitivityY = 15F;
     float moveX = 0F;
@@ -17,13 +19,17 @@ public class Papparazzi : MonoBehaviour {
     public float minimumY = -10F;
     public float maximumY = 10F;
     public float movementSpeed = 10F;
+    Rect rekt;
+    Texture2D[] snap = new Texture2D[3];
+
     // Use this for initialization
     void Start () {
+        rekt = new Rect(Screen.width / 2 - 360, Screen.height / 2 - 240, 720, 480);
         body = GetComponent<Rigidbody>();
         if (body)
             body.freezeRotation = true;
-        originalRotation = transform.localRotation;
         originalPosition = transform.position;
+        shots = maxshots;
 
     }
 	
@@ -35,19 +41,58 @@ public class Papparazzi : MonoBehaviour {
             moveY = Input.GetAxis("Mouse Y") * sensitivityY;
             moveX = Mathf.Clamp(moveX + transform.position.x, minimumX, maximumX) - transform.position.x;
             moveY = Mathf.Clamp(moveY + transform.position.y, minimumY, maximumY) - transform.position.y;
-           transform.Translate(moveX, moveY, 0F);
+            transform.Translate(moveX, moveY, 0F);
+            if (Input.GetMouseButtonDown(0))
+            {
+                canMove = false;
+                Camera.onPostRender +=  Snap;
+            }
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            canMove = false;
-            Snap();
-        }
+        
 
     }
 
-    private void Snap()
+    void OnGUI()
     {
-        //TODO: Snapshot
-        canMove = true;
+        if (shots == 0)
+        {
+            canMove = false;
+
+            if (GUI.Button(new Rect(100, 100, 288, 192), (Texture)snap[0]))
+            {
+
+            }
+            if (GUI.Button(new Rect(400, 100, 288, 192), (Texture)snap[1]))
+            {
+
+            }
+            if (GUI.Button(new Rect(700, 100, 288, 192), (Texture)snap[2]))
+            {
+
+            }
+
+            //TODO: LevelOutro->ChangeLevel
+        }
+    }
+
+    void OnPostRender()
+    {
+        if(shots>0) canMove = true;
+    }
+
+    private void Snap(Camera cam)
+    {
+        Camera.onPostRender -= Snap;
+        if (shots > 0) {
+            //TODO: Snapshot
+            snap[maxshots - shots] = new Texture2D(720, 480);
+            snap[maxshots - shots].ReadPixels(rekt,0,0);
+            snap[maxshots - shots].Apply();
+            shots--;
+            Debug.Log("Shots: " + shots + " Snap Removed " + cam.gameObject.name);
+            //snap[maxshots - shots] = Sprite.Create(tex, rekt, new Vector2(360, 240), 100f, 0, SpriteMeshType.FullRect, Vector4.zero);
+            canMove = true;
+        }
+       
     }
 }

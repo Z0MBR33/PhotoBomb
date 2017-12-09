cb3c46b567b156a7cff32822cc0b41e0f2da0aaf
ga96vaw <ga96vaw@mytum.de>
2017-12-09 11:55:36 +0100

    Added spawner. Added moveMe.cs and testMovingObject.prefab. Confused about files that should be ignored. halp.

---
+++ Assets/spawnObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour {
+
+    public GameObject movingObject;
+    public int minFramesBeforeSpawn = 0;
+    public float minRandomBeforeSpawn = 0.95f;
+    public bool test = false;
+
+    private float check = 0.0f;
+    private int frameCounter = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (frameCounter > minFramesBeforeSpawn)
+        {
+            check = Random.value;
+            Debug.Log(check);
+            if (check > minRandomBeforeSpawn)
+            {
+                Instantiate(movingObject, this.transform);
+                Debug.Log("spawned object");
+            }
+            frameCounter = 0;
+        }
+        frameCounter += 1;
+    }
+}

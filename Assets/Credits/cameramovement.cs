69b15988ab98774c4f947dd794aa742ed9baf976
Martin Beyrle <26486705+made3@users.noreply.github.com>
2017-12-10 11:25:03 +0100

    Credits added

---
+++ Assets/Credits/cameramovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cameramovement : MonoBehaviour {
+
+    public GameObject fatDude;
+    public float zoomOverTime;
+    public float offset;
+    public GameObject pressAnyKey;
+
+    // Use this for initialization
+    void Start () {
+        fatDude.GetComponent<Animator>().SetInteger("randomWalk", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, fatDude.transform.position.z * zoomOverTime + offset);
+        if(fatDude.transform.position.z >= 10)
+        {
+            pressAnyKey.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }else if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Titlescreen");
+        }
+
+	}
+}

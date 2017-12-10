3e0fd4cb85a7f3f306c187ad51f34f4f9878dc0f
ga96vaw <ga96vaw@mytum.de>
2017-12-10 05:29:37 +0100

    added shutter. Instantiate this when triggering photos

---
+++ Assets/makeShutterMotion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeShutterMotion : MonoBehaviour {
+    public GameObject upperShutter;
+    public GameObject lowerShutter;
+    private bool movedown = true;
+    private bool moveup = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (upperShutter.transform.position.y > 10 && movedown)
+            upperShutter.transform.Translate(new Vector3(0.0f, 0.0f, -0.2f));
+        else
+            movedown = false;
+        if (lowerShutter.transform.position.y < -10 && moveup)
+            lowerShutter.transform.Translate(new Vector3(0.0f, 0.0f, 0.2f));
+        else
+            moveup = false;
+
+        if (!movedown)
+            upperShutter.transform.Translate(new Vector3(0.0f, 0.0f, 0.2f));
+        if (!moveup)
+        {
+            lowerShutter.transform.Translate(new Vector3(0.0f, 0.0f, -0.2f));
+        }
+        if (!moveup && !movedown && (!upperShutter.GetComponent<Renderer>().isVisible || !lowerShutter.GetComponent<Renderer>().isVisible))
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
